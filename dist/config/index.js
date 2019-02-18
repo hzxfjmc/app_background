a9e7dbf8c6d125ced2a6cb79b7840885385f2236
azu <hzxfjmc@163.com>
2019-02-18 17:44:03 +0800

    ...

--- dist/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {};
-exports.default = config;
-//# sourceMappingURL=index.js.map
--- src/dbinit.ts
+++ src/dbinit.ts
@@ -1,5 +1,6 @@
 import { InitDbConfig } from './config/db';
 const InitDb = new InitDbConfig();
+//初始化数据库
 // console.log(__dirname + '/models')
 // InitDb.db.addModels([__dirname + '/models'])
 InitDb.db.sync();
